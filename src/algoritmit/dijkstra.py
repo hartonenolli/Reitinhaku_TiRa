6dc70475d586a60472b3e5a2660b111463c39f59
hartonenolli <hartonenolli@gmail.com>
2022-11-18 00:48:07 +0200

    Dijkstran algoritmi toimii melkein

--- src/algoritmit/dijkstra.py
+++ src/algoritmit/dijkstra.py
@@ -4,39 +4,89 @@ import heapq
 class Dijkstra:
     def __init__(self, map_number):
         self.nodes = {}
+        self.neighbours = {}
+        self.visited = []
+        self.dijkstra_map = []
+        self.cordinates = [0,0,9,9]
         self.map = Kartat().maps(map_number)
     
     def get_nodes(self):
         return self.nodes
     
-    def make_nodes(self, number1, number2):
-        for i in range(len(self.map)):
-            for j in range(len(self.map[0])):
-                if str(i) == number1 and str(j) == number2:
-                    print(i, j)
-                    self.nodes[f"{i},{j}"] = 0
-                else:
-                    self.nodes[f"{i},{j}"] = 999
+    def make_nodes(self, start_and_finish):
+        self.cordinates = start_and_finish
+        for y in range(len(self.map)):
+            for x in range(len(self.map[0])):
+                if str(x) == self.cordinates[0] and str(y) == self.cordinates[1]:
+                    #print(y, x)
+                    self.nodes[f"{x},{y}"] = 0
+                    self.neighbours[f"{x},{y}"] = []
+                #else:
+                elif self.map[y][x] != "p":
+                    self.nodes[f"{x},{y}"] = 999
+                    self.neighbours[f"{x},{y}"] = []
+                self.make_neighbour(x,y)
         #print(self.nodes)
+        #self.make_neighbour()
+        #self.algorithim()
+        #print(self.neighbours)
         self.algorithim()
+
+    def make_neighbour(self, x, y):
+        #for y in range(len(self.map)):
+        #    for x in range(len(self.map[0])):
+        if self.map[y][x] == "o":
+            #Handle different directions
+            #left
+            if x > 0:
+                if self.map[y][x-1] != "p":
+                    self.neighbours[f"{x},{y}"].append(f"{x-1},{y}")
+            #up
+            if y > 0:
+                if self.map[y-1][x] != "p":
+                    self.neighbours[f"{x},{y}"].append(f"{x},{y-1}")
+            #right
+            if x < len(self.map[0])-1:
+                if self.map[y][x+1] != "p":
+                    self.neighbours[f"{x},{y}"].append(f"{x+1},{y}")
+            #down
+            if y < len(self.map)-1:
+                if self.map[y+1][x] != "p":
+                    self.neighbours[f"{x},{y}"].append(f"{x},{y+1}")
+        #print(self.neighbours)
 
     def printtaa(self, number):
         print(self.map)
         self.make_nodes()
 
     def algorithim(self):
-        keko = []
-        heapq.heappush(keko, ("0,0", self.nodes.get("0,0")))
-        print(keko)
-        #while keko != []:
-        solmu = keko.pop()[0]
-        print(solmu)
-        #    if kasitelty[solmu]
-        #        continue
-        #    kasitelty[solmu] = true
-        #    for kaari in verkko[solmu]
-        #        nyky = etaisyys[kaari.loppu]
-        #        uusi = etaisyys[solmu]+kaari.paino
-        #        if uusi < nyky
-        #            etaisyys[kaari.loppu] = uusi
-        #            keko.push((uusi,kaari.loppu))
+        #print(self.neighbours)
+        heap = []
+        heapq.heappush(heap, (f"{self.cordinates[0]},{self.cordinates[1]}", self.nodes.get(f"{self.cordinates[0]},{self.cordinates[1]}")))
+        #print(heap)
+        target = f"{self.cordinates[2]},{self.cordinates[3]}"
+        while heap != []:
+            #print(heap)
+            #print(min(heap))
+            knot = min(heap)
+            heap.remove(min(heap))
+            #print(knot[0])
+            #print(self.neighbours.get(knot[0]))
+            if knot[0] in self.visited:
+                #print("visited")
+                continue
+            self.visited.append(knot[0])
+            for next in self.neighbours.get(knot[0]):
+                #print("täällä")
+                #print(self.neighbours.get(knot[0]))
+                now = self.nodes[next]
+                new = self.nodes[knot[0]]+1
+                #print(new, now)
+                if new < now:
+                    self.nodes[next] = new
+                    heapq.heappush(heap, (next, new))
+                    #heap.push((new,next))
+                    #print("mentiin")
+        #print(new)
+        #print(target)
+        print(self.nodes[target])
--- src/karttaruutu.py
+++ src/karttaruutu.py
@@ -9,6 +9,7 @@ class KarttaRuutu:
         self._frame = None
         self.canvas = None
         self.x_y_list = ["0", "0", "9", "9"]
+        self.save_dijkstra = None
         self._initialize()
 
     def pack(self):
@@ -16,6 +17,9 @@ class KarttaRuutu:
 
     def destroy(self):
         self._frame.destroy()
+    
+    def set_dijkstra(self):
+        self.save_dijkstra = 1
 
     """Alustetaan kartta käymällä rivejä läpi,
     jos kartalla on 'o', niin tulee valkoinen,
@@ -23,33 +27,42 @@ class KarttaRuutu:
     Lisäksi, jos valkoisella on valittu alku koordinaatti,
     niin tulee sininen. Punainen tulee loppukoordinaatille."""
 
-    def set_cordinates_for_canvas(self, map_number, algorithm_number):
+    def set_cordinates_for_canvas(self, map_number):
         if self.canvas is not None:
             self.canvas.after(1000, self.canvas.destroy())
         self.canvas = Canvas(self._root, width=500, height=500)
+        if not self.save_dijkstra is None:
+            pass
         show_map = Kartat().maps(map_number)
         color1 = 0
         color2 = 0
 
         start1 = ""
         start2 = ""
+        finish1 = ""
+        finish2 = ""
 
         for i in range(40, 401, 40):
             for j in range(40, 401, 40):
                 if show_map[color1][color2] == "o":
                     self.canvas.create_rectangle(
                         j, i, j+40, i+40, fill="white")
-                    if self.x_y_list is not None:
-                        if str(color1
-                               ) == self.x_y_list[0] and str(color2) == self.x_y_list[1]:
-                            self.canvas.create_rectangle(
-                                j, i, j+40, i+40, fill="blue")
-                            start1 += str(color1)
-                            start2 += str(color2)
-                        if str(color1
-                               ) == self.x_y_list[2] and str(color2) == self.x_y_list[3]:
-                            self.canvas.create_rectangle(
-                                j, i, j+40, i+40, fill="red")
+                    #if self.x_y_list is not None:
+                    if str(color1
+                            ) == self.x_y_list[0] and str(color2) == self.x_y_list[1]:
+                        self.canvas.create_rectangle(
+                            j, i, j+40, i+40, fill="blue")
+                        start1 += str(color2)
+                        start2 += str(color1)
+                    if str(color1
+                            ) == self.x_y_list[2] and str(color2) == self.x_y_list[3]:
+                        self.canvas.create_rectangle(
+                            j, i, j+40, i+40, fill="red")
+                        finish1 += str(color2)
+                        finish2 += str(color1)
+                elif show_map[color1][color2] == "d":
+                    self.canvas.create_rectangle(
+                        j, i, j+40, i+40, fill="green")
                 else:
                     self.canvas.create_rectangle(
                         j, i, j+40, i+40, fill="black")
@@ -57,16 +70,17 @@ class KarttaRuutu:
             color1 += 1
             color2 = 0
         self.canvas.pack(padx=0, pady=50)
-        if algorithm_number == 1:
-            Dijkstra(map_number).make_nodes(start1, start2)
+        #if algorithm_number == 1:
+        if self.save_dijkstra == 1:
+            Dijkstra(map_number).make_nodes([start1, start2, finish1, finish2])
         return True
 
-    def _handle_finding_route(self, value_list, map_number, algorithm_number):
+    def _handle_finding_route(self, value_list, map_number):
         print("Reitti löytyi ajassa:")
         print(value_list[0], value_list[1])
         print(value_list[2], value_list[3])
         self.x_y_list = value_list
-        self.set_cordinates_for_canvas(map_number, algorithm_number)
+        self.set_cordinates_for_canvas(map_number)
 
     """Alustetaan näkymä."""
 
@@ -95,7 +109,7 @@ class KarttaRuutu:
             master=self._frame,
             text="Kartta1",
             command=lambda: self._handle_finding_route(
-                [y_1.get(), x_1.get(), y_2.get(), x_2.get()], 1, 1)
+                [y_1.get(), x_1.get(), y_2.get(), x_2.get()], 1)
         )
 
         button.grid(row=1, column=0)
@@ -105,6 +119,12 @@ class KarttaRuutu:
                                 command=lambda:
                                 self._handle_finding_route(
                                     [y_1.get(), x_1.get(), y_2.get(), x_2.get()],
-                                    2, 1))
+                                    2))
 
         button_map.grid(row=1, column=1)
+
+        button_dijkstra = ttk.Button(master=self._frame,
+                                text="Dijkstra",
+                                command=self.set_dijkstra)
+        
+        button_dijkstra.grid(row=1, column=2)
